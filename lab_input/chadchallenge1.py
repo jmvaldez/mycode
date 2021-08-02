05a4d479d8780ffe73e10b17995528d54171074a
Joseph Valdez <joseph.m.valdez1@gmail.com>
2021-08-02 18:01:24 +0000

    Chad Challenge 1

---
+++ lab_input/chadchallenge1.py
@@ -0,0 +1,8 @@
+## input asking for user's name
+user_name = input("Enter your username:")
+
+## input asking what day of the week it is
+day_of_week = input("Enter the current day of week:")
+
+## Print statement that reads outputs of both inputs
+print("Hello" +"," , user_name + "!", "Happy", day_of_week + "!") 
